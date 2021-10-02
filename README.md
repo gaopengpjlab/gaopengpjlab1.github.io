8306a3148a7636e9d136fa2a154217d717955f50
gaopengcuhk <51525797+gaopengcuhk@users.noreply.github.com>
2021-10-02 17:41:47 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# jiangzhengkai.github.io
---
+++ files/button.js
@@ -0,0 +1,19 @@
+/* 点击按钮，下拉菜单在 显示/隐藏 之间切换 */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// 点击下拉菜单意外区域隐藏
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
